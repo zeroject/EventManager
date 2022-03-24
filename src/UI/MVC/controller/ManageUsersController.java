fd529efad2de8149bf4992c40df47f0020186eb8
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2022-03-24 16:57:48 +0100

    halfway done with adding users to list

--- src/UI/MVC/controller/ManageUsersController.java
+++ src/UI/MVC/controller/ManageUsersController.java
@@ -1,12 +1,32 @@
 package UI.MVC.controller;
 
+import UI.MVC.model.UserModel;
 import UI.Utility.CREATESCENE;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class ManageUsersController {
 
+    @FXML
+    private TableView tvUsers;
+    @FXML
+    private TableColumn tcFirstName;
+    @FXML
+    private TableColumn tcLastName;
+    @FXML
+    private TableColumn tcMNumber;
+    @FXML
+    private TableColumn tcEmail;
+    @FXML
+    private TableColumn tcIsAdmin;
+    @FXML
+    private TableColumn tcIsManager;
+    @FXML
+    private TableColumn tcID;
     private CREATESCENE CREATESCENE = new CREATESCENE();
 
     @FXML
@@ -24,5 +44,19 @@ public class ManageUsersController {
     @FXML
     public void edit(){
         CREATESCENE.createScene("../view/EditView.fxml", "file:../CSS-Files/main.css", false, this);
+    }
+
+    public void initialize(){
+        tvUsers.setPlaceholder(new Label("No users found in database"));
+
+        tcFirstName.setCellValueFactory();
+        tcLastName.setCellValueFactory();
+        tcMNumber.setCellValueFactory();
+        tcEmail.setCellValueFactory();
+        tcIsAdmin.setCellValueFactory();
+        tcIsManager.setCellValueFactory();
+        tcID.setCellValueFactory();
+
+        tvUsers.setItems(UserModel.getAllManagers());
     }
 }
