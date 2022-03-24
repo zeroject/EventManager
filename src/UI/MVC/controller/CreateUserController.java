719d339b8788433bbd59cc56ac3db2f9baa71385
zeroject <47432671+zeroject@users.noreply.github.com>
2022-03-24 17:16:42 +0100

    Create User view done

--- src/UI/MVC/controller/CreateUserController.java
+++ src/UI/MVC/controller/CreateUserController.java
@@ -1,4 +1,22 @@
 package UI.MVC.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
 public class CreateUserController {
+
+    public TextField userFirstNameField;
+    public TextField userLastNameField;
+    public TextField userMobilNumberField;
+    public TextField userEmailField;
+    public CheckBox isAdminCheck;
+    public CheckBox isEventCordinatorCheck;
+
+    public CreateUserController(){
+        userFirstNameField = new TextField();
+        userLastNameField = new TextField();
+    }
 }
