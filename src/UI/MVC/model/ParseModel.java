df5aed28dcbee64bb11c0e8043ade3edbf130d5b
zeroject <47432671+zeroject@users.noreply.github.com>
2022-04-04 16:04:57 +0200

    Update ParseModel.java

--- src/UI/MVC/model/ParseModel.java
+++ src/UI/MVC/model/ParseModel.java
@@ -1,4 +1,11 @@
 package UI.MVC.model;
 
+import BE.Event;
+import BE.Guest;
+import BE.User;
+
 public class ParseModel {
+    public static Event event;
+    public static Guest guest;
+    public static User user;
 }
