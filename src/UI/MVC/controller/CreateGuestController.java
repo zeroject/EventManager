64d83bb2b3a4ab43b279d6886d69c8a5ac616848
Ominousity <abs@hotmail.dk>
2022-04-06 14:16:43 +0200

    added combobox to createGuest

--- src/UI/MVC/controller/CreateGuestController.java
+++ src/UI/MVC/controller/CreateGuestController.java
@@ -1,18 +1,28 @@
 package UI.MVC.controller;
+import BE.Event;
+import UI.MVC.model.EventModel;
 import UI.MVC.model.GuestTicketModel;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
 import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class CreateGuestController
+public class CreateGuestController implements Initializable
 {
-    @FXML TextField tfFornavn;
-    @FXML TextField tfEfternavn;
-    @FXML TextField tfEmail;
-    @FXML TextField tfMobilNummer;
-    @FXML TextField tfVoksen;
-    @FXML TextField tfChild;
+    private EventModel eventModel;
+
+    @FXML private ComboBox<Event> eventBox;
+    @FXML private TextField tfFornavn;
+    @FXML private TextField tfEfternavn;
+    @FXML private TextField tfEmail;
+    @FXML private TextField tfMobilNummer;
+    @FXML private TextField tfVoksen;
+    @FXML private TextField tfChild;
 
     private GuestTicketModel guestTicketModel;
 
@@ -25,11 +35,17 @@ public class CreateGuestController
         tfVoksen = new TextField();
         tfChild = new TextField();
         guestTicketModel = new GuestTicketModel();
+        eventModel = new EventModel();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        eventBox.setItems(eventModel.getAllEvents());
     }
 
     @FXML
     public void createGuest() throws SQLException
     {
-        //guestTicketModel.createGuest(tfFornavn.getText(), tfEfternavn.getText(), tfMobilNummer.getText(), tfEmail.getText(), Integer.parseInt(tfVoksen.getText()), Integer.parseInt(tfChild.getText()));
+        guestTicketModel.createGuest(eventBox.getSelectionModel().getSelectedItem().getId(), tfFornavn.getText(), tfEfternavn.getText(), tfMobilNummer.getText(), tfEmail.getText(), Integer.parseInt(tfVoksen.getText()), Integer.parseInt(tfChild.getText()));
     }
 }
