c0169a183a78dfb717a894e410f50724bf581000
Zeroject <cypeyt@gmail.com>
2022-04-22 10:40:57 +0200

    Commetns

--- src/UI/MVC/model/GuestTicketModel.java
+++ src/UI/MVC/model/GuestTicketModel.java
@@ -17,16 +17,36 @@ public class GuestTicketModel {
         guestManager = new GuestTIcketManager();
     }
 
+    /**
+     * Får en liste med alle Gæster som er i det event med det givet ID
+     * @param eventID
+     * @return ObservableList med GuestTickets
+     */
     public ObservableList<GuestTicket> getAllGuestsInEvent(int eventID) {
         guestTicketList.clear();
         guestTicketList.addAll(guestManager.getAllGuestsInEvent(eventID));
         return guestTicketList;
     }
 
+    /**
+     * Funktion som sender parameternes værdier til guestManager
+     * @param eventID
+     * @param firstName
+     * @param lastName
+     * @param mNumber
+     * @param email
+     * @param adultNum
+     * @param childNum
+     * @throws SQLException
+     */
     public void createGuest(int eventID, String firstName, String lastName, String mNumber, String email, int adultNum, int childNum) throws SQLException {
         guestManager.createGuest(eventID, firstName, lastName, mNumber, email, adultNum, childNum);
     }
 
+    /**
+     * Sender ID vidre til guestManager som skal fjernes
+     * @param guestID
+     */
     public void deleteGuest(int guestID){
         guestManager.deleteGuest(guestID);
     }
--- src/UI/MVC/model/UserModel.java
+++ src/UI/MVC/model/UserModel.java
@@ -18,18 +18,37 @@ public class UserModel {
         userManager = new UserManager();
     }
 
+    /**
+     * Får en liste af alle users der er i programmet
+     * @return ObservableList med Users i
+     */
     public ObservableList<User> getAllUsers(){
         userList.clear();
         userList.addAll(userManager.getAllUsers());
         return userList;
     }
 
+    /**
+     * Funktion som sender paremeterns værdier vidre til userManager for at lave en ny user
+     * @param username
+     * @param password
+     * @param email
+     * @throws SQLException
+     */
     public void createUser(String username, String password, String email) throws SQLException {
         userManager.createUser(username, password, email);
     }
 
+    /**
+     * Sender en user ned til userManager der skal updateres
+     * @param user
+     */
     public void updateUser(User user){userManager.updateUser(user);}
 
+    /**
+     * Sender et ID ned til userManager som skal fjernes
+     * @param userID
+     */
     public void deleteUser(int userID){
         userManager.deleteUser(userID);
     }
